7f440c265787bbbe077a4be33d93dc85584b1836
dongxuehong <529975613@qq.com>
2020-12-23 12:23:58 +0800

    commit工程到本地仓库

---
+++ idea-git-test/src/com/dxh/git/FirstGitTestClass.java
@@ -0,0 +1,12 @@
+package com.dxh.git;
+
+public class FirstGitTestClass {
+    public static void main(String[] args) {
+        System.out.println("hello git test project !");
+        System.out.println("hello git test project !");
+        System.out.println("hello git test project !");
+        System.out.println("hello git test project !");
+        System.out.println("hello git test project !");
+        System.out.println("hello git test project !");
+    }
+}
