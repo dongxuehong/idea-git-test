da5a4bebab74aba5303bf0e0a59147e852240865
dongxuehong <529975613@qq.com>
2020-12-23 13:11:01 +0800

    开发者2修改代码

--- idea-git-test/src/com/dxh/git/FirstGitTestClass.java
+++ idea-git-test/src/com/dxh/git/FirstGitTestClass.java
@@ -8,9 +8,11 @@ public class FirstGitTestClass {
         System.out.println("hello git test project !");
         System.out.println("hello git test project !");
         System.out.println("hello git test project !");
+        //修改代码，增加注释
     }
     public void test(){
         int i=0;
         i+=1;
+        System.out.println(i);
     }
 }
